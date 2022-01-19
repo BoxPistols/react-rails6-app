38bb12fec9e3f52424300fca3ae9ac89c596fe5e
aito <BoxPistols@users.noreply.github.com>
2022-01-20 03:37:36 +0900

    React move

---
+++ app/javascript/components/HellWorld.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+// class HellWorld extends React.Component {
+//   render () {
+//     return (
+//       <React.Fragment>
+//         Greeting: {this.props.greeting}
+//       </React.Fragment>
+//     );
+//   }
+// }
+const HellWorld = () => {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+
+HellWorld.propTypes = {
+  greeting: PropTypes.string
+};
+
+export default HellWorld
