2f1735e2529f7690a0f485d5173372626f57a1f2
aito <BoxPistols@users.noreply.github.com>
2022-01-19 19:17:09 +0900

    API用設定 記述リファクト

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::TodosController < ApplicationController
+
+	# 一覧
   def index
+		# todos変数(複数) = モデルTodoのインスタンス、ソート追加古い順
     todos = Todo.order(updated_at: :desc)
+		# jsonのtodosから取得
     render json: todos
   end
 
+	# 詳細
   def show
+		# todos変数(単数) = モデルTodoのインスタンス、そのID該当のデータを探す
     todo = Todo.find(params[:id])
+		# jsonのtodosから取得
     render json: todo
   end
 
+	# 新規作成
   def create
     todo = Todo.new(todo_params)
     if todo.save
@@ -17,7 +25,7 @@ class Api::V1::TodosController < ApplicationController
       render json: todo.errors, status: 422
     end
   end
-
+	# 編集更新
   def update
     todo = Todo.find(params[:id])
     if todo.update(todo_params)
@@ -26,7 +34,7 @@ class Api::V1::TodosController < ApplicationController
       render json: todo.errors, status: 422
     end
   end
-
+	# 削除
   def destroy
     if Todo.destroy(params[:id])
       head :no_content
@@ -34,7 +42,7 @@ class Api::V1::TodosController < ApplicationController
       render json: { error: "Failed to destroy" }, status: 422
     end
   end
-
+	# 全体削除
   def destroy_all
     if Todo.destroy_all
       head :no_content
@@ -44,7 +52,7 @@ class Api::V1::TodosController < ApplicationController
   end
 
   private
-
+	# セキュア
   def todo_params
     params.require(:todo).permit(:name, :is_completed)
   end
