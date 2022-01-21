e2d7e2d67f85ada5e38088102437160a73f2184f
aito <BoxPistols@users.noreply.github.com>
2022-01-21 19:18:11 +0900

    Re yarn on M1

---
+++ app/javascript/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import styled from 'styled-components'
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+import EditTodo from './EditTodo'
+import Nav from './Nav';
+import './App.css'
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`
+
+export default function App() {
+	return (
+		<>
+			<Router>
+				<Logo>Logo</Logo>	
+				{/* Nav Bar */}
+				<Nav />
+				<Wrapper>
+					<Routes>
+						<Route exact path="todos" component={<TodoList />} />
+						<Route exact path="todos/new" component={<AddTodo />} />
+						<Route path="todos/:id/edit" component={<EditTodo />} />
+					</Routes>
+				</Wrapper>
+			</Router>
+		</>
+	);
+}
